aea68735962fe06077458fb7cf1b6dc7fbb4f37f
zihad07 <zihadnayem8gmail.com>
2020-01-18 23:12:23 +0600

    Created Model -a

--- E-commerce-rest-api/routes/web.php
+++ E-commerce-rest-api/routes/web.php
@@ -14,3 +14,8 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::apiResource('/products','ProductController');
+
+Route::apiResource('/products/{product}/reviews','ReviewController');
